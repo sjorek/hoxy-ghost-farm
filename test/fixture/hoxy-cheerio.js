cf9fccbd53c8b88fc5b23e309e96ccb37aa32bce
Stephan Jorek <stephan.jorek@gmail.com>
2012-12-02 19:45:16 +0100

    updated submodule and tweaked test-fixtures

--- test/fixture/hoxy-cheerio.js
+++ test/fixture/hoxy-cheerio.js
@@ -1,5 +1,6 @@
 
 // $('base').attr('href','http://www.dgb.de.eve:8080/');
+// $('base').attr('href','http://www.dgb.de.eve/');
 
 $('link[rel="stylesheet"], script, noscript').remove();
 $('head').prepend('<meta http-equiv="Content-Type" content="text/html; charset=utf8">');
