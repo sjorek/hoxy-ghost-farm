c1a76d99bd079f3debebc816b18b50def6bf6a70
Stephan Jorek <stephan.jorek@gmail.com>
2012-12-02 17:47:15 +0100

    update

--- hoxy-cheerio.js
+++ hoxy-cheerio.js
@@ -1,3 +1,5 @@
 
 // $('base').attr('href','http://www.dgb.de.eve:8080/');
 
+$('link[rel="stylesheet"], script, noscript').remove();
+$('head').prepend('<meta http-equiv="Content-Type" content="text/html; charset=utf8">');
